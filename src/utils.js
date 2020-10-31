414419475b2b061eac0b896d50860a514f5c4f2f
Andreas Steiner <andreassteiner@gmx.de>
2020-10-31 19:46:38 +0100

    Fixes new7d.

--- src/utils.js
+++ src/utils.js
@@ -20,6 +20,20 @@ export const rowidx = Object.fromEntries(
 export const colidx = Object.fromEntries(
   cols.map((name, idx) => [name, idx]))
 
+const RunningAverage = length => {
+  const data = Array(length).fill(0)
+  let n = 0, sum = 0
+  return {
+    get: () => sum / Math.min(7, n),
+    add: value => {
+      const i = n++ % data.length
+      sum -= data[i]
+      data[i] = value
+      sum += data[i]
+    },
+  }
+}
+
 function extend(table) {
   const t0 = new Date().getTime()
   const new7d = {}
@@ -28,13 +42,14 @@ function extend(table) {
   const deaths = {}
   const augmented = table.map(date => date.map((row, idx) => {
     if (!row) return
-    new7d[idx] = (new7d[idx] || 0)*6/7 + row[colidx.new]/7
+    if (!new7d[idx]) new7d[idx] = RunningAverage(7)
+    new7d[idx].add(row[colidx.new])
     confirmed[idx] = (confirmed[idx] || 0) + row[colidx.new]
     recovered[idx] = (recovered[idx] || 0) + row[colidx.drecovered]
     deaths[idx]    = (   deaths[idx] || 0) + row[colidx.ddeaths]
     const active = confirmed[idx] - deaths[idx] - recovered[idx]
     const fatality = deaths[idx] / confirmed[idx]
-    return [...row, Math.round(new7d[idx]), confirmed[idx], recovered[idx], deaths[idx], active, fatality]
+    return [...row, Math.round(new7d[idx].get()), confirmed[idx], recovered[idx], deaths[idx], active, fatality]
   }))
   console.log('extend.dt', new Date().getTime() - t0)
   return augmented
